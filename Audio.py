7ec433b8ab51f07d92c0f9a19a05aff4006e7672
Rubén <74114026+rubzip@users.noreply.github.com>
2024-04-08 15:51:02 +0200

    Add files via upload

--- Audio.py
+++ Audio.py
@@ -1,17 +1,35 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
+import pyaudio
+import wave
 
 
 class Audio:
     def __init__(self, duration: int, path: str):
-        self.fs = 88000
-        self.duration = duration
-        self.path = path
-        self.recording = None
-
+        self.chunk = 1024  # Record in chunks of 1024 samples
+        self.sample_format = pyaudio.paInt16  # 16 bits per sample
+        self.channels = 2
+        self.fs = 44100  # Record at 44100 samples per second
+        self.seconds = duration
+        self.filename = path
+        
     def record(self):
-        self.recording = sd.rec(int(self.duration * self.fs), samplerate=self.fs, channels=2)
-        self.wait()
+        p = pyaudio.PyAudio()
+        stream = p.open(format=self.sample_format,
+                channels=self.channels,
+                rate=self.fs,
+                frames_per_buffer=self.chunk,
+                input=True)
+        frames = []
+        for i in range(0, int(self.fs / self.chunk * self.seconds)):
+            data = stream.read(chunk)
+            frames.append(data)
+
+        # Stop and close the stream 
+        stream.stop_stream()
+        stream.close()
+        # Terminate the PortAudio interface
+        p.terminate()
+
+
     
     def save(self):
         try:
